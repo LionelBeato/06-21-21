ea92f3e32b550f72e01900df70ca89ef8e682a7f
Lionel Beato <lionel.beato@gmail.com>
2021-06-30 12:07:16 -0400

    wrap up of js lesson

--- day_7/script.js
+++ day_7/script.js
@@ -28,6 +28,8 @@ console.log(`the value of x is ${x}`)
 console.log("the value of y is " + y)
 console.log(y)
 
+// const are not reassignable
+// however, they are mutable
 const MY_CONST = 7;
 
 console.log(MY_CONST)
@@ -48,7 +50,7 @@ console.log(myString);
 
 // below we have a number
 // numbers will generally respect arithmetic 
-let myNumber = 100; 
+let myNumber = 100;
 let myDecimalValue = 0.45;
 
 // below are boolean values
@@ -58,6 +60,130 @@ let myDecimalValue = 0.45;
 let myBoolean = true;
 let myOtherBoolean = false;
 
+// TODO(write comment) 
+let myVariable = "abc";
+console.log(typeof myVariable);
+
+myVariable = 1
+console.log(typeof myVariable);
+
+let coercedVariable = 7 + 7 + 7
+console.log(`the value of coercedVariable is ${coercedVariable}`);
+
+coercedVariable = 7 + 7 + "7"
+console.log(`the value of coercedVariable is ${coercedVariable}`);
+
+coercedVariable = "7" + 7 + 7
+console.log(`the value of coercedVariable is ${coercedVariable}`);
 
 
 
+let parsedInt = parseInt("10");
+console.log(parsedInt);
+
+let foo = null;
+
+console.log(`This is the value of foo: ${foo}`)
+
+
+let conditional = false;
+
+if (conditional) {
+    console.log("The conditional was true.")
+} else {
+    console.log("The conditional was false.")
+}
+
+let numberConditional = 2;
+
+if (numberConditional === 5) {
+    console.log("The number is equal to 5")
+} else if (numberConditional > 3) {
+    console.log("The number is at least bigger than 3")
+} else if (numberConditional < 10) {
+    console.log("The number is less than 10")
+}
+
+let equalityCheckNumber = 10;
+
+if (equalityCheckNumber == "10") {
+    console.log("The values are equal")
+}
+
+if (equalityCheckNumber === "10") {
+    console.log("The values and types are equal");
+}
+
+if (true && false) {
+    console.log("This code will never be reached");
+}
+
+if (true && true) {
+    console.log("This code will always be reached");
+}
+
+if (true || false) {
+    console.log("as long as one of the conditions are true, this code will be reached.")
+}
+
+// this "i" is specific the the for loop, and it won't affect
+// other variables declared as i
+// i++ is the same as saying i = i + 1
+for (let i = 0; i <= 9; i++) {
+    console.log(i);
+}
+
+// challenge 3 starter code
+
+for (let i = 99; i > 0; i--) {
+    console.log(`${i} bottles of beer on the wall`)
+}
+
+let whileLoopVariable = 0;
+
+while (whileLoopVariable <= 10) {
+    console.log(whileLoopVariable);
+    whileLoopVariable = whileLoopVariable + 1;
+}
+
+// while (true) {
+//     console.log("this will break my computer")
+// }
+
+// do-while loops will always resolve at least once
+do {
+    console.log("this will always print to the console.")
+} while (false);
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if (i === 5) {
+        break;
+    }
+}
+
+for (let i = 0; i < 10; i++) {
+
+    if (i === 3) {
+        continue;
+    }
+
+    console.log(i);
+    if (i === 5) {
+        break;
+    }
+}
+
+
+let switchVariable = 44;
+
+switch (switchVariable) {
+    case 1:
+        console.log("The variable is set to 1");
+        break;
+    case 43:
+        console.log("The variable is indeed 43!");
+        break;
+    default:
+        console.log("No cases were matched!");
+}
