4779b2a5e1b706151246c155be43f936b173c54d
Lionel Beato <lionel.beato@gmail.com>
2021-08-02 12:24:22 -0400

    fix state hook

--- week_7/day_1/my-movies/src/components/Movie.jsx
+++ week_7/day_1/my-movies/src/components/Movie.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const Movie = ({ title, description, year, genre }) => {
+  const [state, setState] = useState(0);
+
+  const handleClick = () => {
+    setState(state + 1);
+  };
+
   return (
     <div>
       <h4>{title}</h4>
       <h4>{description}</h4>
       <h4>{year}</h4>
       <h4>{genre}</h4>
+      <p>{state}</p>
+      <button onClick={handleClick}>click me</button>
       <hr></hr>
     </div>
   );
